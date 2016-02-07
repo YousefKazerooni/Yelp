9483013fe878055c1414115abba93a5e14fd9598
YousefKazerooni <gityousefkazerooni@gmail.com>
2016-02-06 19:22:46 -0800

    completed searchBar (+ the cancel button)

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,21 +8,35 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     var businesses: [Business]!
+    var businessesBackup: [Business]!
+    var searchBar: UISearchBar!
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //searchBar added programatically into the navigation bar
+        searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
+        
         //After setting up the height using autolayout in the storyboard
         //we must specify here that the height must change
         //***must always be used in conjuction with estimatedrowheight
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
+        
+        
+        
+        
         
 
         Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
@@ -72,6 +86,60 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         
         return cell
     }
+    
+    
+    //Note: All credits for the following function
+    //go to kmolo (Katherine Monoley)
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if(businessesBackup == nil) {
+            businessesBackup = businesses
+        }
+        
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            businesses = businessesBackup
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            businesses = businesses.filter({(dataItem: Business) -> Bool in
+                
+                // If dataItem matches the searchText, return true to include it
+                if dataItem.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    
+    //************ showing the cancel button upon editing in the search bar
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    //*********** activating the cancel button
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        
+        //After removing cancel we must reload the view
+        businesses = businessesBackup
+        self.tableView.reloadData()
+        
+    }
+    
+    
+    
 
     /*
     // MARK: - Navigation
